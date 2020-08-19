79a7e3f80951b2be336577856c081b6e4a34423d
Lawangin Khan <lawangin.khan@gmail.com>
2020-08-19 12:28:36 -0400

    first commit of hello

---
+++ src/com/lawanginkhan/cmsc255/lab1/Hello.java
@@ -0,0 +1,20 @@
+/********************************
+ * Lab 1, Hello
+ ********************************
+ * Lawangin Khan
+ * August 19, 2020
+ * CMCS 255 section C90
+ *******************************/
+
+package com.lawanginkhan.cmsc255.lab1;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Lawangin Khan");
+        System.out.println("Lab 1, Hello");
+        System.out.println("Print out your hello message.");
+        System.out.println("CMCS 255 section C90");
+        System.out.println();
+    }
+
+}
